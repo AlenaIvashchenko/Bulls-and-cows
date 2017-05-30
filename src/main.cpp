b0500895ed8918ed475c12593bf1bc7a8456567d
AlenaIvashchenko <alenshik1997god@mail.ru>
2017-05-30 23:57:58 +0700

    Creat main

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <ctime>
+#include <stdlib.h>
+#include <stdio.h>
+#include "conio.h"
+#include "logic.h"
+#include "display.h"
+
+int main() {
+    srand(time(NULL));
+    clearScreen();
+    int step = 0;
+
+    int compNum[4];
+    makeNumber(compNum);
+
+    bool exitFlag = false;
+    while (exitFlag != true) {
+        int inputNum = 0;
+        printf(" Write your number: "); scanf("%d", &inputNum);
+
+        // Если игрок победил
+        if (checkBeasts(compNum, inputNum, step)) {
+            printf(" Want to play more? (Y/N): ");
+            char key = getch();
+            switch (key) {
+                case 'Y': case 'y':
+                    step = 0; clearScreen(); break;
+                case 'N': case 'n':
+                    exitFlag = true; break;
+                default:
+                    clearScreen(); break;
+            }
+        }
+    }
+}
