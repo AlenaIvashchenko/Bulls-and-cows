346ff4461bee851faedb9b4a396750a87097102c
AlenaIvashchenko <alenshik1997god@mail.ru>
2017-06-11 09:23:33 +0700

    Display.h

---
+++ src/display.h
@@ -0,0 +1,8 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+void clearScreen();
+void printNumber(const int number[]);
+void printBeasts(const int bulls, const int cows, const int step);
+
+#endif // DISPLAY_H
